1dbde3e4d2229339754014c85b767a0aaf525b49
aulia <kingmalprax@gmail.com>
2017-01-05 19:09:19 +0700

    install bootstrap and simple_form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
 
-  get 'home/dashboard'
+  get '/dashboard', :to => "home#dashboard", :as => :dashboard
+  get '/upcoming_washes', :to => "home#upcoming_washes", :as => :upcoming_washes
 
-  get 'home/upcoming_washes'
+  root :to => 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
